a128f1e35548d09b6b298827fdf4f91083073740
ViceElez <vice.elezovic12@gmail.com>
2024-11-08 16:03:11 +0100

    dodavanje 4 zadatka u branch pod imenon 4.zad koji je napravljen lokalno

---
+++ zad4/zad4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Unesite broj redaka za dijamant:");
+        int n = int.Parse(Console.ReadLine());
+        
+        for (int i = 1; i <= n; i += 2)
+        {
+            int spaces = (n - i) / 2;
+            for (int j = 1; j < spaces+1; j++)
+            {
+                Console.Write(' ');
+            }
+
+            for (int k = 1; k <= i; k++)
+            {
+                    Console.Write("*");
+            }
+            Console.WriteLine();
+           
+        }
+        for (int i = n - 2; i > 0; i -= 2)
+        {
+            int spaces = (n - i) / 2;
+            for (int j = 1; j < spaces+1; j++)
+            {
+                Console.Write(' ');
+            }
+
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
