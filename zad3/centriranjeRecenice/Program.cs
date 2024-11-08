1a425b9d2690667a9abf3f3b4560f1b2ca50e5f2
ViceElez <vice.elezovic12@gmail.com>
2024-11-08 15:59:26 +0100

    dodavanje 3 zadatka u braacnh pod imenon 3.zad koji je napravljen lokalno

---
+++ zad3/centriranjeRecenice/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Upisite zeljenu sirinu linije:");
+        int n= int.Parse(Console.ReadLine());
+        Console.WriteLine("Unesite zeljen broj znakova po liniji:");
+        int m=int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Upisite tekst koji zelite centrirati:");
+        string text = Console.ReadLine();
+        Console.WriteLine();
+
+
+        List<string> words = new List<string>();
+        words=LineFormatting(text, m);
+        PrintCentered(words, n);
+    }
+
+    static List<string> LineFormatting(string text, int m)
+    {
+        List<string> allLines = new List<string>();
+        string[] words = text.Split(' ');
+        
+        string currLine="";
+
+        foreach (string word in words)
+        {
+                if((currLine+word).Length>m)
+                {
+                    allLines.Add(currLine);
+                    currLine = "";
+
+                }
+                currLine += word + " ";
+        }
+
+        if (string.IsNullOrEmpty(currLine) == false)
+        {
+            allLines.Add(currLine);
+        }
+
+        return allLines;
+    }
+    
+    static void PrintCentered(List<string> allLines, int n)
+    {
+        foreach (string line in allLines)
+        {
+            int spaces = (n - line.Length) / 2;
+            string formattedLine = new string(' ', spaces) + line;
+            Console.WriteLine(formattedLine);
+        }
+    }
+
+    
+}
